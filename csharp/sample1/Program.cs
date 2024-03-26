14062c19a53608dbc2da45f8850986ceae672d86
Adam Pickering <adam.pickering@suse.com>
2024-03-26 13:43:55 -0600

    Add csharp/

---
+++ csharp/sample1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HelloWorld;
+using System.IO;
+
+var asdf = new MyClass();
+asdf.MyNewFunc();
+asdf.ReadFile();
+
+namespace HelloWorld
+{
+	class MyClass
+	{
+
+		public void ReadFile()
+		{
+			string contents = File.ReadAllText("test.txt");
+			Console.WriteLine(contents);
+		}
+		public void MyNewFunc()
+		{
+			string[] messages = {
+				"hello world",
+				"volvo",
+				"apple",
+				"pear",
+			};
+			foreach (string message in messages)
+			{
+				if (message == "apple")
+				{
+					break;
+				}
+				Console.WriteLine(message);
+			}
+		}
+	}
+}
