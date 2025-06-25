4d8a136f698a24bfae93b399ddec23c8e8a54b96
Adam Pickering <adam.pickering@suse.com>
2025-06-25 17:10:59 -0600

    Add golang/orasReference

---
+++ golang/orasReference/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"oras.land/oras-go/v2/registry"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	rawRefs := []string{
+		"flannelcni/flannel",
+		"docker.io/flannelcni/flannel",
+		"docker.io/rancher/mirrored-flannelcni-flannel:v0.16.0",
+		"registry.suse.com/rancher/mirrored-flannelcni-flannel:v0.16.0",
+	}
+	for _, rawRef := range rawRefs {
+		ref, err := registry.ParseReference(rawRef)
+		if err != nil {
+			return fmt.Errorf("failed to parse %q: %w", rawRef, err)
+		}
+		fmt.Printf("%s host: %s\n", rawRef, ref.Host())
+		fmt.Printf("%s -> {Registry: %s, Repository: %s, Reference: %s}\n", rawRef, ref.Registry, ref.Repository, ref.Reference)
+	}
+
+	return nil
+}
